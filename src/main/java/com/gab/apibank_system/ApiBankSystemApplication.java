b4dd1436271223ce663031e57ce6dc587000992e
gabriel <gabriel.donascimento@jala.university>
2025-06-23 17:46:46 -0300

    first commit

---
+++ src/main/java/com/gab/apibank_system/ApiBankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gab.apibank_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBankSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBankSystemApplication.class, args);
+    }
+
+}
